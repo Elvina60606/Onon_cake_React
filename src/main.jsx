cdcd3cd727a3664244a432c681d79a7a21a11170
Elvina <cat250070@gmail.tw>
2026-01-27 02:17:17 +0800

    加入優惠卷與點數頁面

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
@@ -9,8 +8,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </StrictMode>,
 )
