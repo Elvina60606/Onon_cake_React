259fd1caef01f9a56d9b518851f17a2bd74ad10f
Elvina <cat250070@gmail.tw>
2026-01-31 13:38:20 +0800

    加入熱銷排行區塊

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,9 @@ import rectangle303 from '../assets/images/rectangle303.png';
 import cake from '../assets/images/cake.png';
 import cake2 from '../assets/images/cake2.png';
 import cake3 from '../assets/images/cake3.png';
+import canele from '../assets/images/canele.png';
+import lemonMadeleine from '../assets/images/lemonMadeleine.png';
+import basque from '../assets/images/basque.png';
 
 const Home = () => {
 
@@ -133,34 +136,121 @@ const Home = () => {
                      alt="裝飾向量圖案1" />
             </section>
         {/* 品牌故事區塊 */}
-            <section class="story-container">
-                <div class="story-content text-primary-800">
-                    <h3 class="fs-lg-1 fs-3 mb-6 fw-bold lh-48 story-title">品牌故事</h3>
-                    <p class="fw-normal lh-24 mb-4">
+            <section className="story-container">
+                <div className="story-content text-primary-800">
+                    <h3 className="fs-lg-1 fs-3 mb-6 fw-bold lh-48 story-title">品牌故事</h3>
+                    <p className="fw-normal lh-24 mb-4">
                         2020年，從日本結束兩年的甜點研習後回到台灣，心中始終懷抱著一個小小夢想 —— 擁有一家屬於自己的甜點店。於是，ONON CAKE 二溫菓子 誕生了。
                     </p>
-                    <p class="fw-normal lh-24 mb-2">2022｜二溫菓子 品牌創立</p>
-                    <p class="fw-normal lh-24 mb-2">2023｜中壢中原分店 開幕</p>
-                    <p class="fw-normal lh-24 mb-4">2024｜桃園藝文分店 開幕</p>
-                    <p class="fw-normal">在平淡的生活中，用甜點找回你的心跳</p>
+                    <p className="fw-normal lh-24 mb-2">2022｜二溫菓子 品牌創立</p>
+                    <p className="fw-normal lh-24 mb-2">2023｜中壢中原分店 開幕</p>
+                    <p className="fw-normal lh-24 mb-4">2024｜桃園藝文分店 開幕</p>
+                    <p className="fw-normal">在平淡的生活中，用甜點找回你的心跳</p>
                 </div>
-                    <img class="cake-img" src={cake} alt="杯子蛋糕" />
-                    <img class="cake2-img" src={cake2} alt="切片蛋糕" />
-                    <img class="cake3-img" src={cake3} alt="法國麵包" />
-                    <img class="vector11-img"
+                    <img className="cake-img" src={cake} alt="杯子蛋糕" />
+                    <img className="cake2-img" src={cake2} alt="切片蛋糕" />
+                    <img className="cake3-img" src={cake3} alt="法國麵包" />
+                    <img className="vector11-img"
                          src={vector1}
                          alt="裝飾向量圖案1" />
-                    <img class="vector2-img"
+                    <img className="vector2-img"
                          src={vector2}
                          alt="裝飾向量圖案2" />
-                    <img class="vector3-img"
+                    <img className="vector3-img"
                          src={vector3}
                          alt="裝飾向量圖案3" />
-                    <img class="rectangle303-img"
+                    <img className="rectangle303-img"
                          src={rectangle303}
                          alt="背景波浪圖案"/>
             </section>
-
+        {/* 熱銷排行 */}
+            <section className="bg-secondary-50">
+                <div className="index-container">
+                    <div className="text-center pb-6 pt-8 py-lg-8">
+                        <h1 className="fs-3 fs-lg-1">熱銷排行</h1>
+                    </div>
+                    <div className="row row-cols-1 row-cols-md-3 g-6">
+                        <div className="col mb-2 mb-lg-8">
+                            <div className="card card-shadow rounded-xl h-100 overflow-hidden">
+                                <span className="badge rank-badge fs-6">第一名</span>
+                                <div className="overflow-hidden">
+                                    <img src={canele}
+                                         className="card-img-top dessert-card"
+                                         alt="經典原味可麗露" />
+                                </div>
+                                <div className="card-body p-4">
+                                    <h4 className="pb-2 text-primary-800">經典原味可麗露</h4>
+                                    <p className="fs-6 text-primary-600 pb-4">
+                                        外酥內嫩焦糖香，法式經典一口入魂
+                                    </p>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <p className="h4 text-primary-700">NT$ 180</p>
+                                        <button type="button"
+                                                className="btn btn-outline-secondary rounded-pill btn-card-shopping-cart">
+                                            <span className="material-symbols-outlined fill">
+                                                shopping_cart
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-2 mb-lg-8">
+                            <div className="card card-shadow rounded-xl h-100 overflow-hidden">
+                                <span className="badge rank-badge fs-6">第二名</span>
+                                <div className="overflow-hidden">
+                                    <img src={lemonMadeleine}
+                                        className="card-img-top dessert-card"
+                                        alt="糖漬檸檬瑪德蓮"/>
+                                </div>
+                                <div className="card-body p-4">
+                                    <h4 className="pb-2 text-primary-800">糖漬檸檬瑪德蓮</h4>
+                                    <p className="fs-6 text-primary-600 pb-4">
+                                        清香檸檬糖漬點綴，鬆軟口感酸甜平衡剛剛好
+                                    </p>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <p className="h4 text-primary-700">NT$ 150</p>
+                                        <button type="button"
+                                                className="btn btn-outline-secondary rounded-pill btn-card-shopping-cart">
+                                            <span className="material-symbols-outlined fill">   
+                                                shopping_cart 
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-6 mb-lg-8">
+                            <div className="card card-shadow rounded-xl h-100 overflow-hidden">
+                                <span className="badge rank-badge fs-6">第三名</span>
+                                <div className="overflow-hidden">
+                                    <img src={basque}
+                                         className="card-img-top dessert-card"
+                                         alt="超濃厚巴斯克" />
+                                </div>
+                                <div className="card-body p-4">
+                                    <h4 className="pb-2 text-primary-800">超濃厚巴斯克</h4>
+                                    <p className="fs-6 text-primary-600 pb-4">
+                                        香氣濃烈滑順綿密，入口即化的極致濃厚感
+                                    </p>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <p className="h4 text-primary-700">NT$ 240</p>
+                                        <button type="button"
+                                                className="btn btn-outline-secondary rounded-pill btn-card-shopping-cart">
+                                            <span className="material-symbols-outlined fill"> 
+                                                shopping_cart 
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center pb-8">
+                        <a href="product.html" className="btn btn-light fs-6">更多商品</a>
+                    </div>
+                </div>
+            </section>
 
 
 
