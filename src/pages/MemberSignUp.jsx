d9954e4b50ab78502d5688cf829d9677aaf10473
evashen <misseva0718@gmail.com>
2026-02-05 23:17:19 +0800

    新增會員註冊頁jsx

---
+++ src/pages/MemberSignUp.jsx
@@ -0,0 +1,48 @@
+import HeroBanner from "@/components/signup/HeroBanner";
+import MemberInfoSection from "@/components/signup/MemberInfoSection";
+import PasswordSection from "@/components/signup/PasswordSection";
+import TermsSection from "@/components/signup/TermsSection";
+import SignUpSuccessModal from "@/components/signup/SignUpSuccessModal";
+
+
+const MemberSignUp=() => {
+  return (
+    <>     
+      <HeroBanner />
+
+      <main className="bg-secondary-50">
+        <div className="container py-md-12 py-8">
+          <div className="row g-6">
+            <div className="col-12 col-lg-6">
+              {/* 會員資料 */}
+              <MemberInfoSection />
+            </div>
+
+            <div className="col-12 col-lg-6">
+              <div className="d-flex flex-column gap-6">
+                {/* 設定密碼 & 會員須知 */}
+                <PasswordSection />
+                <TermsSection />
+              </div>
+            </div>
+          </div>
+          {/* 確認送出btn */}
+          <div className="col-12 mt-8">
+            <button
+              className="btn btn-primary w-100 py-3"
+              type="submit"
+              data-bs-toggle="modal"
+              data-bs-target="#signUpConfirm"
+            >
+              確認送出
+            </button>
+          </div>
+        </div>
+        {/* 註冊成功modal */}
+        <SignUpSuccessModal />
+      </main>    
+    </>
+  );
+}
+
+export default MemberSignUp;
